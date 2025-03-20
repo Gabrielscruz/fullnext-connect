86bd26c049a3d527df7758b94b4220bc79586b82
gabriel.silva <Gabriel.Silva@wocc.com.br>
2025-03-20 08:30:18 -0300

    organizacao

--- src/app/organization/page.tsx
+++ src/app/organization/page.tsx
@@ -84,7 +84,6 @@ export default function Organization({ searchParams }: searchParamsProps) {
     const [step, setStep] = useState(1);
     const totalSteps = 5;
 
-    /*
     useEffect(() => {
         if (status === "unauthenticated" ) {
             if (searchParams?.email === undefined) {
@@ -93,7 +92,7 @@ export default function Organization({ searchParams }: searchParamsProps) {
             
         }
     }, [status, router]);
-*/
+
     if (status === "loading" || !session) return null;
 
     function toggleSelection(list: string[], setList: Function, option: string) {
